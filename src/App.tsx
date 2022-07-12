0dee2944bd251ef8182b47862712132cf128579f
Sriram Chamarthy <sriram2491999@gmail.com>
2022-07-13 00:19:51 +0530

    feat: added page structure and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import DividerDesktop from "./assets/pattern-divider-desktop.svg";
+import DividerMobile from "./assets/pattern-divider-mobile.svg";
+import DiceIcon from "./assets/icon-dice.svg";
 
 function App() {
-  return <h1 className="text-3xl-blue font-bold underline">Hello world!</h1>;
+  return (
+    // Page
+    <div className="flex flex-1 items-center justify-center h-screen bg-darkBlue">
+      {/* Middle Box */}
+      <div className="flex flex-col relative min-h-52 max-w-60 rounded-xl m-3 py-5 px-10 items-center text-center bg-darkGrayishBlue md:w-96">
+        {/* Content - title, quote, divider */}
+        <div className="flex flex-1 flex-col mb-8">
+          {/* title */}
+          <span className="text-sm tracking-widest text-neonGreen">
+            ADVICE #177
+          </span>
+
+          {/* quote  */}
+          <p className="my-5 flex flex-1 text-lg items-center text-lightCyan font-extrabold">
+            "It's easy to sit up and take notice,what's difficult is to
+            stand-up"
+          </p>
+
+          {/* Divider desktop */}
+          <img
+            src={DividerDesktop}
+            alt="divider desktop"
+            className="hidden md:flex"
+          />
+          {/* Divider mobile */}
+          <img
+            src={DividerMobile}
+            alt="divider moile"
+            className="flex md:hidden"
+          />
+        </div>
+
+        {/* Shuffle button  */}
+        <button className="flex absolute -bottom-5 p-3 rounded-full bg-neonGreen hover:drop-shadow-3xl">
+          <img src={DiceIcon} alt="dice" className="h-5 w-5" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      md: "375px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        sans: ["Manrope"],
+      },
+      dropShadow: {
+        "3xl": "0 0 20px hsl(150, 100%, 66%)",
+      },
+    },
   },
   plugins: [],
 };
